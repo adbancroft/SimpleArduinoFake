10b6666b377248ba28f66ae54446b0ac05932b40
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-03-19 09:58:43 -0500

    Should include 'this' parameter (for consistency/just in case these classes are derived from)

--- src/EEPROMFake.cpp
+++ src/EEPROMFake.cpp
@@ -4,14 +4,14 @@
 // clang-format on
 
 uint8_t EEPROMClass::read(int idx) {
-  return ArduinoFakeInstance(EEPROM)->read(idx);
+  return ArduinoFakeInstance(EEPROM, this)->read(idx);
 };
 void EEPROMClass::write(int idx, uint8_t val) {
-  ArduinoFakeInstance(EEPROM)->write(idx, val);
+  ArduinoFakeInstance(EEPROM, this)->write(idx, val);
 };
 void EEPROMClass::update(int idx, uint8_t val) {
-  ArduinoFakeInstance(EEPROM)->update(idx, val);
+  ArduinoFakeInstance(EEPROM, this)->update(idx, val);
 };
-uint16_t EEPROMClass::length() { return ArduinoFakeInstance(EEPROM)->length(); }
+uint16_t EEPROMClass::length() { return ArduinoFakeInstance(EEPROM, this)->length(); }
 
 EEPROMClass EEPROM = EEPROMFakeProxy(ArduinoFakeInstance(EEPROM));
--- src/SPIFake.cpp
+++ src/SPIFake.cpp
@@ -1,28 +1,28 @@
 #include "ArduinoFake.h"
 #include "SPIFake.h"
 
-void SPIClass::begin() { ArduinoFakeInstance(SPI)->begin(); };
+void SPIClass::begin() { ArduinoFakeInstance(SPI, this)->begin(); };
 
-void SPIClass::end() { ArduinoFakeInstance(SPI)->end(); };
+void SPIClass::end() { ArduinoFakeInstance(SPI, this)->end(); };
 
 void SPIClass::beginTransaction(SPISettings settings) {
-  ArduinoFakeInstance(SPI)->beginTransaction(settings);
+  ArduinoFakeInstance(SPI, this)->beginTransaction(settings);
 };
 
 void SPIClass::endTransaction(void) {
-  ArduinoFakeInstance(SPI)->endTransaction();
+  ArduinoFakeInstance(SPI, this)->endTransaction();
 };
 
 uint8_t SPIClass::transfer(uint8_t data) {
-  return ArduinoFakeInstance(SPI)->transfer(data);
+  return ArduinoFakeInstance(SPI, this)->transfer(data);
 };
 
 uint16_t SPIClass::transfer16(uint16_t data) {
-  return ArduinoFakeInstance(SPI)->transfer16(data);
+  return ArduinoFakeInstance(SPI, this)->transfer16(data);
 };
 
 void SPIClass::transfer(void *buf, size_t count) {
-  return ArduinoFakeInstance(SPI)->transfer(buf, count);
+  return ArduinoFakeInstance(SPI, this)->transfer(buf, count);
 };
 
 SPIClass SPI = SPIFakeProxy(ArduinoFakeInstance(SPI));
