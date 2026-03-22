1c97897d5108e660a880271e644ec61d79de0c15
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-03-21 22:54:22 -0500

    StreamFakeProxy: remove unused methods

--- src/StreamFake.h
+++ src/StreamFake.h
@@ -16,41 +16,4 @@ public:
     StreamFakeProxy(TBase* fake) : PrintFakeProxy<TBase>(fake)
     {
     }
-
-    size_t write(uint8_t value) override
-    {
-        return getFake()->write(value);
-    }
-
-    int available() override
-    {
-        return getFake()->available();
-    }
-
-    int read() override
-    {
-        return getFake()->read();
-    }
-
-    int peek() override
-    {
-        return getFake()->peek();
-    }
-
-    void flush() override
-    {
-        getFake()->flush();
-    }
-
-    void setTimeout(unsigned long timeout) override
-    {
-        getFake()->setTimeout(timeout);
-    }
-
-    unsigned long getTimeout(void) override
-    {
-        return getFake()->getTimeout();
-    }
-
-    using PrintFakeProxy<TBase>::getFake;
 };
