ed7ac177adf00c04900efd459fe64c4de82666ba
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-05-30 14:38:17 -0500

    Copy analog pin definitions from Mega Arduino code

--- src/arduino/pins_arduino.h
+++ src/arduino/pins_arduino.h
@@ -47,23 +47,39 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 
 #define LED_BUILTIN 13
 
-#define PIN_A0   (14)
-#define PIN_A1   (15)
-#define PIN_A2   (16)
-#define PIN_A3   (17)
-#define PIN_A4   (18)
-#define PIN_A5   (19)
-#define PIN_A6   (20)
-#define PIN_A7   (21)
+#define PIN_A0   (54)
+#define PIN_A1   (55)
+#define PIN_A2   (56)
+#define PIN_A3   (57)
+#define PIN_A4   (58)
+#define PIN_A5   (59)
+#define PIN_A6   (60)
+#define PIN_A7   (61)
+#define PIN_A8   (62)
+#define PIN_A9   (63)
+#define PIN_A10  (64)
+#define PIN_A11  (65)
+#define PIN_A12  (66)
+#define PIN_A13  (67)
+#define PIN_A14  (68)
+#define PIN_A15  (69)
 
-static const uint8_t A0 = PIN_A0;
-static const uint8_t A1 = PIN_A1;
-static const uint8_t A2 = PIN_A2;
-static const uint8_t A3 = PIN_A3;
-static const uint8_t A4 = PIN_A4;
-static const uint8_t A5 = PIN_A5;
-static const uint8_t A6 = PIN_A6;
-static const uint8_t A7 = PIN_A7;
+constexpr uint8_t A0 = PIN_A0;
+constexpr uint8_t A1 = PIN_A1;
+constexpr uint8_t A2 = PIN_A2;
+constexpr uint8_t A3 = PIN_A3;
+constexpr uint8_t A4 = PIN_A4;
+constexpr uint8_t A5 = PIN_A5;
+constexpr uint8_t A6 = PIN_A6;
+constexpr uint8_t A7 = PIN_A7;
+constexpr uint8_t A8 = PIN_A8;
+constexpr uint8_t A9 = PIN_A9;
+constexpr uint8_t A10 = PIN_A10;
+constexpr uint8_t A11 = PIN_A11;
+constexpr uint8_t A12 = PIN_A12;
+constexpr uint8_t A13 = PIN_A13;
+constexpr uint8_t A14 = PIN_A14;
+constexpr uint8_t A15 = PIN_A15;
 
 #define digitalPinToPCICR(p)    (((p) >= 0 && (p) <= 21) ? (&PCICR) : ((uint8_t *)0))
 #define digitalPinToPCICRbit(p) (((p) <= 7) ? 2 : (((p) <= 13) ? 0 : 1))
